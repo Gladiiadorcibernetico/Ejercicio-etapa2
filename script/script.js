55891912e9743b063e2737bed63606b30fe982b1
EstebanGilabert <gilabertesteban2022@gmail.com>
2022-06-29 02:08:49 -0500

    Agregar informacion de repasso JS

--- script/script.js
+++ script/script.js
@@ -1,1 +1,16 @@
-alert("Hola")
+let nombrePlanetas = Array('Tierra','Marte','Jupiter','Neptuno');
+let distancia = [6,3,7];
+const tamaño = [];
+
+for (let index = 0; index < nombrePlanetas.length; index++){
+    console.log(nombrePlanetas[index]);
+}
+
+for (const key in nombrePlanetas){
+    if (Object.hasOwnProperty.call(nombrePlanetas, key)){
+        const element = nombrePlanetas[key];
+    }
+}
+
+
+console.log('In')
